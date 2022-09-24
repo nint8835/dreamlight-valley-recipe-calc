6c2feea1707544dd8c7f64a6732c123b280d9b5a
Riley Flynn <riley@rileyflynn.me>
2022-09-24 12:09:06 -0230

    Add basic soufflé calculator

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dreamlight Valley Recipe Calculator
+
+This project is an app to calculate the costs, ingredient quantities, and profit amounts for recipes in Disney Dreamlight Valley.
+
+Currently, it only supports one specific use case, which is cooking Soufflés with ingredients bought from Remy and coal you mined yourself as this is my current method of grinding for money (and the inspiration behind this project, as I wanted to know for certain how many of each ingredient to buy to use up all the money I had), but the plan is to expand this to support more ingredients, recipes, and stores / stalls in the future.
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,86 @@
+import { AppNavBar } from 'baseui/app-nav-bar';
+import { Block } from 'baseui/block';
+import { Card, StyledBody, StyledTitle } from 'baseui/card';
+import { StyledDivider } from 'baseui/divider';
+import { FormControl } from 'baseui/form-control';
+import { CheckIndeterminate } from 'baseui/icon';
+import { Input } from 'baseui/input';
+import { ListItem, ListItemLabel } from 'baseui/list';
+import { Table } from 'baseui/table-semantic';
 import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
+import { useStyletron } from 'styletron-react';
+
+const ingredientCosts = {
+    butter: 190,
+    cheese: 180,
+    egg: 220,
+    milk: 230,
+};
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [css] = useStyletron();
+
+    const [initialInvestmentStr, setInitialInvestmentStr] = useState('');
+
+    const investmentAmountInt = parseInt(initialInvestmentStr) || 0;
+
+    const souffleUnitCost =
+        ingredientCosts.butter + ingredientCosts.cheese + ingredientCosts.egg + ingredientCosts.milk;
+    const souffleCount = Math.floor(investmentAmountInt / souffleUnitCost);
 
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </div>
+        <Block
+            color="contentPrimary"
+            backgroundColor="backgroundPrimary"
+            width={'100vw'}
+            height={'100vh'}
+            overflow={'auto'}
+        >
+            <AppNavBar title="Dreamlight Valley Recipe Calculator" />
+            <Block padding="1.5rem">
+                <Card>
+                    <StyledTitle>Soufflé</StyledTitle>
+                    <StyledBody>
+                        <FormControl
+                            label={() => 'Initial investment'}
+                            caption={() => 'Number of Star Coins to invest into purchasing ingredients'}
+                        >
+                            <Input
+                                type="number"
+                                value={initialInvestmentStr}
+                                onChange={(e) => setInitialInvestmentStr(e.target.value)}
+                            />
+                        </FormControl>
+                        <StyledDivider />
+                        <Table
+                            columns={['Ingredient', 'Quantity', 'Unit Cost', 'Total Cost']}
+                            data={[
+                                ['Butter', souffleCount, ingredientCosts.butter, ingredientCosts.butter * souffleCount],
+                                ['Cheese', souffleCount, ingredientCosts.cheese, ingredientCosts.cheese * souffleCount],
+                                ['Egg', souffleCount, ingredientCosts.egg, ingredientCosts.egg * souffleCount],
+                                ['Milk', souffleCount, ingredientCosts.milk, ingredientCosts.milk * souffleCount],
+                            ]}
+                        />
+                        <StyledDivider />
+                        <ul className={css({ paddingLeft: 0, paddingRight: 0 })}>
+                            <ListItem artwork={(props) => <CheckIndeterminate {...props} />}>
+                                <ListItemLabel description={`${souffleCount} soufflés`}>Soufflé Count</ListItemLabel>
+                            </ListItem>
+                            <ListItem artwork={(props) => <CheckIndeterminate {...props} />}>
+                                <ListItemLabel description={`${1200 * souffleCount} Star Coins`}>
+                                    Sale Value
+                                </ListItemLabel>
+                            </ListItem>
+                            <ListItem artwork={(props) => <CheckIndeterminate {...props} />}>
+                                <ListItemLabel description={`${(1200 - souffleUnitCost) * souffleCount} Star Coins`}>
+                                    Profit
+                                </ListItemLabel>
+                            </ListItem>
+                        </ul>
+                    </StyledBody>
+                </Card>
+            </Block>
+        </Block>
     );
 }
 
